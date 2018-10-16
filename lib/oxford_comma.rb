049ceec62f110e3128c5500e55109e5002fd9144
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-16 15:03:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.length == 2 
+  if array.length == 1 
+    array.to_s
+  elsif array.length == 2 
     array.join(" and ")
   elsif array.length == 3
     array[0,1].join(", ") << array[2].join(" and ")
