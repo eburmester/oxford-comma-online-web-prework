a2a8880aedbc188feec2eea91a9bdd56863c0b49
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-16 15:16:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
     array.join(" and ")
-  else
-    array.join(", ") << array[array.length-1, -1].join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
 end
